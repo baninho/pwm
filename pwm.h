398e27728e9f420b8471d1c42014a5b99a43f493
baninho <florianimbt@gmail.com>
2016-10-23 13:02:55 +0200

    refacotr

--- pwm.h
+++ pwm.h
@@ -4,7 +4,7 @@
 #include <wiringPi.h>
 #include <stdio.h>
 
-int pwn(unsigned int intPin, unsigned int per_microseconds, float *dutyCycle, unsigned int cycles);
+int pwn(unsigned int intPin, unsigned int per_microseconds, float dutyCycle, unsigned int cycles);
 int an_aus(unsigned int intPin, unsigned int per_microseconds, unsigned int t_high);
 
 #endif
