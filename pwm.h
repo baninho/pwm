22615e36f4ba937bb95ed3d1d46659f6e554d903
baninho <florianimbt@gmail.com>
2016-10-23 12:36:43 +0200

    remove obsolete extern

--- pwm.h
+++ pwm.h
@@ -4,7 +4,7 @@
 #include <wiringPi.h>
 #include <stdio.h>
 
-extern int pwn(unsigned int, unsigned int, float*, unsigned int);
-extern int an_aus(unsigned int, unsigned int, unsigned int);
+int pwn(unsigned int, unsigned int, float*, unsigned int);
+int an_aus(unsigned int, unsigned int, unsigned int);
 
 #endif
