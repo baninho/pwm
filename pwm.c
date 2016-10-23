6b8dcb8e85b348c3c94917db1228cd78ac299dbf
baninho <florianimbt@gmail.com>
2016-10-23 12:39:49 +0200

    rename argument per to per_microseconds

--- pwm.c
+++ pwm.c
@@ -1,22 +1,22 @@
 #include "pwm.h"
 
-int pwm(unsigned int intPin, unsigned int per, float *dutyCycle, 
+int pwm(unsigned int intPin, unsigned int per_microseconds, float *dutyCycle, 
 unsigned int cycles) {
 	
 	unsigned int t_high;
 	unsigned int i;	
 
-	t_high = (int)(*dutyCycle * (float)per);
+	t_high = (int)(*dutyCycle * (float)per_microseconds);
 	i = 0;
 	printf("Pin %d\n",intPin);
 	printf("t_high %d\n",t_high);
 	printf("duty %f\n",*dutyCycle);
-	printf("period %d\n",per);
+	printf("period %d\n",per_microseconds);
 	printf("cycles %d\n",cycles);
 	
 	while (i < cycles) {
 		
-		if(an_aus(intPin, per, t_high) == -1) {break;}
+		if(an_aus(intPin, per_microseconds, t_high) == -1) {break;}
 		++i;
 	}
 	
@@ -26,9 +26,9 @@ unsigned int cycles) {
 
 }
 
-int an_aus(unsigned int intPin, unsigned int per, unsigned int t_high) {
+int an_aus(unsigned int intPin, unsigned int per_microseconds, unsigned int t_high) {
 	
-	if (t_high > per)
+	if (t_high > per_microseconds)
 		return -1;
 
 	if (t_high > 0) {
@@ -36,9 +36,9 @@ int an_aus(unsigned int intPin, unsigned int per, unsigned int t_high) {
 		delayMicroseconds(t_high);
 	}
 
-	if (t_high < per) {
+	if (t_high < per_microseconds) {
 		digitalWrite(intPin, 0);
-		delayMicroseconds(per-t_high);
+		delayMicroseconds(per_microseconds-t_high);
 	}
 
 	return 0;
